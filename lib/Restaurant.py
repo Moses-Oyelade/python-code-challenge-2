6f56752afd80432844fde805d765b048b6c82f7e
Moses-Oyelade <oyelademoses@gmail.com>
2023-10-20 16:09:35 +0100

    aggregate and relationship methods

--- lib/Restaurant.py
+++ lib/Restaurant.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import sessionmaker, relationship, backref
 
 Base =declarative_base()
 
+
+
 rest_customer = Table(
     'rest_customers',
     Base.metadata,
@@ -16,8 +18,10 @@ class Restaurant(Base):
     __tablename__ = 'restaurants'
     
     id = Column(Integer(), primary_key = True)
-    name = Column(String)
-    price = Column(Integer)
+    name = Column(String())
+    reg_number = Column(String())
+    price = Column(Integer())
+    
     
     reviews = relationship('Review', backref=backref('restaurant'))
     customers =relationship('Customer', secondary =rest_customer, back_populates=('restaurants'))
@@ -25,16 +29,18 @@ class Restaurant(Base):
     def __repr__(self):
         return f'Reataurant(id={self.id}: ' +\
             f'name={self.name}, ' +\
+            f'reg_number={self.reg_number}, ' +\
             f'price={self.price})'
+   
     
 
 class Customer(Base):
     __tablename__ = 'customers'
     
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    gender = Column(String)
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String())
+    last_name = Column(String())
+    gender = Column(String())
     
     reviews = relationship('Review', backref=backref('customer'))
     restaurants =relationship('Restaurant', secondary =rest_customer, back_populates=('customers'))
@@ -45,3 +51,21 @@ class Customer(Base):
             f'last_name={self.last_name}, ' +\
             f'gender={self.gender})'
     
+
+            
+class Review(Base):
+    __tablename__ = 'reviews'
+    
+    id = Column(Integer(), primary_key=True)
+    rating = Column(Integer())
+    restaurant_id = Column(Integer(), ForeignKey('restaurants.id'))
+    customer_id = Column(Integer(), ForeignKey('customers.id'))
+    
+   
+
+
+
+engine = create_engine('sqlite:///restaurants.db', echo=True)
+Base.metadata.create_all(bind=engine)
+Session=sessionmaker(bind=engine)
+session = Session()
